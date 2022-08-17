ecfb0ade798628867aa615cea2360be61dc85af3
Adebakin Yusuf <109008836+Yusufxcode@users.noreply.github.com>
2022-08-17 04:41:25 +0100

    getting computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,3 +12,16 @@ const getUserChoice = (userInput) => {
       console.log('please enter valid Input');
     }
   }
+
+  const getComputerChoice = () => {
+    const random = Math.floor(Math.random() * 3);
+    if (random === 0){
+      return 'rock';
+    }else if (random === 1){
+      return 'paper';
+    }else if (random === 2){
+      return 'scissors';
+    }else {
+      console.log('invalid');
+    }
+  }
